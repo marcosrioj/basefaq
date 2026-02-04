40a9f97e9a0d3f1b835099438858c3165888e97b
Marcos Lima <marcosrioj@gmail.com>
2026-02-04 01:10:50 -0800

    Inital migration added

--- dotnet/BaseFaq.Faq.FaqWeb.App/Extensions/ServiceCollectionExtensions.cs
+++ dotnet/BaseFaq.Faq.FaqWeb.App/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,7 @@
 using BaseFaq.Common.Infrastructure.Core.Abstractions;
 using BaseFaq.Common.Infrastructure.Core.Services;
+using BaseFaq.Faq.FaqWeb.Business.Faq.Extensions;
+using BaseFaq.Faq.FaqWeb.Persistence.FaqDb.Extensions;
 
 namespace BaseFaq.Faq.FaqWeb.App.Extensions;
 
@@ -7,6 +9,8 @@ public static class ServiceCollectionExtensions
 {
     public static void AddFeatures(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddFaqDb(configuration.GetConnectionString("FaqDb"));
+        services.AddFaqBusiness();
         //services.AddEventsFeature();
     }
 
